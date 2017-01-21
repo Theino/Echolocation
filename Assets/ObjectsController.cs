e978754688141a29b1c9d2d7b1aa794170150a10
Theino <theino8@yahoo.com>
2017-01-21 08:59:51 -0500

    GoalObject triggers working and adding new GoalObject randomly

---
+++ Assets/ObjectsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsController : MonoBehaviour {
+
+    public GameObject GoalObject;
+    public float WorldSize = 20f;
+
+    private GameObject player;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void handleCollision(GameObject collidedObject)
+    {
+        Debug.Log("handling collision");
+        GameObject newGoalObject = Instantiate(GoalObject);
+        float newX = Random.Range(-WorldSize / 2f, WorldSize / 2f);
+        float newY = Random.Range(-WorldSize / 2f, WorldSize / 2f);
+        float newZ = Random.Range(-WorldSize / 2f, WorldSize / 2f);
+        newGoalObject.transform.position = new Vector3(newX, newY, newZ);
+        Destroy(collidedObject);
+    }
+}
---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour {
+
+    public GameObject Objects;
+
+    private GameObject collidedObject;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+	}
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("trigger entered");
+        collidedObject = other.gameObject;
+        Transform collidedParent = collidedObject.transform.parent;
+        if (collidedParent.gameObject == Objects)
+        {
+            ObjectsController objectsController = (ObjectsController) Objects.GetComponent(typeof(ObjectsController));
+            objectsController.handleCollision(collidedObject);
+        }
+    }
+}
