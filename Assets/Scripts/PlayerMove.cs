176334248f965fa20038da6ef1605fe62165c08b
Theino <theino8@yahoo.com>
2017-01-21 07:28:47 -0500

    3D Splash and movement working

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,26 +2,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityStandardAssets.Characters.FirstPerson;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerMove : MonoBehaviour {
 
+    public float XSensitivity = 2f;
+    public float YSensitivity = 2f;
+    public GameObject Camera;
+    public GameObject Player;
+    public float movementSpeed = 1f;
+
     private float timeLastPressed;
     private float timeDelayBetweenClicks = 1;
-    private MouseLook mouseLook = new MouseLook();
+    private Quaternion cameraRot;
+    private Quaternion playerRot;
 
 	private void Start () {
         timeLastPressed = Time.time;
+        cameraRot = Camera.transform.localRotation;
+        playerRot = Player.transform.localRotation;
 	}
 	
 	private void Update () {
-        CheckForMove();
+        float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * XSensitivity;
+        float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * YSensitivity;
         
-	}
+        cameraRot *= Quaternion.Euler(-xRot, 0f, 0f);
+        playerRot *= Quaternion.Euler(0f, yRot, 0f);
+
+        Camera.transform.localRotation = cameraRot;
+        Player.transform.localRotation = playerRot;
+    }
 
     private void FixedUpdate()
     {
-        
+        transform.position += transform.forward * movementSpeed;
     }
 
     private void CheckForMove()
