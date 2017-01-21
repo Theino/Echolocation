f5a4a71d4bfc24974cd824cd9a1f4ffa00dfb668
Theino <theino8@yahoo.com>
2017-01-21 00:08:13 -0500

    No Changes

---
+++ Assets/Scripts/Background/WorldMap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Background
+{
+    enum mapTypes : int {empty=0, filled};
+    class WorldMap
+    {
+        const int size = 10;
+        public int[,] map = new int[size,size];
+        Random rand;
+
+        WorldMap()
+        {
+            rand = new Random();
+            fillMap();
+        }
+
+        void fillMap()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    map[i, j] = GetRandomSpace();
+                }
+            }
+        }
+
+        int GetRandomSpace()
+        {
+            int typeCount = Enum.GetNames(typeof(mapTypes)).Length;
+            return rand.Next(typeCount);
+        }
+    }
+}
