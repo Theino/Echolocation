f1dba4a8e08b65da05f5db6ccc95e2ef20f00798
Theino <theino8@yahoo.com>
2017-01-22 01:09:53 -0500

    working sound controller for fish

--- Assets/ObjectsController.cs
+++ Assets/ObjectsController.cs
@@ -8,51 +8,79 @@ public class ObjectsController : MonoBehaviour {
     public float WorldSize = 20f;
     public int maxFish = 3;
 
-    private GameObject player;
     private AudioSource capturedSource;
+
+    private GameObject player;
+   
     private float lastTime;
-    private int currFish = 0;
+    private float startTime;
+    private Queue<GameObject> objectsToPlay;
+    private Queue<GameObject> waitingObjectsToPlay;
 
     // Use this for initialization
     void Start () {
         player = GameObject.Find("Player");
         capturedSource = transform.GetComponent<AudioSource>();
-
-        createNewFish();
+        objectsToPlay = new Queue<GameObject>();
+        waitingObjectsToPlay = new Queue<GameObject>();
+        for(int i = 0; i < maxFish; i++)
+            createNewFish();
         lastTime = Time.time;
+        startTime = 0;
     }
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
     {
-        if (currFish < maxFish)
+        if (startTime < Time.time - 4)
         {
-            if (lastTime < Time.time - 0.5)
-            {
-                createNewFish();
-                lastTime = Time.time;
-            }
+            Debug.Log("starting");
+            objectsToPlay = new Queue<GameObject>(waitingObjectsToPlay);
+            waitingObjectsToPlay = new Queue<GameObject>();
+            startTime = Time.time;
         }
-	}
+        if (lastTime < Time.time - 1)
+        {
+            Debug.Log("lastTime");
+            Debug.Log(objectsToPlay.Count.ToString());
+            if (objectsToPlay.Count > 0)
+            {
+                GameObject playingObject = objectsToPlay.Dequeue();
+                while (objectsToPlay.Count > 0 && playingObject == null)
+                    playingObject = objectsToPlay.Dequeue();
+                if (playingObject != null)
+                {
+                    Debug.Log("play sound");
+                    playingObject.GetComponent<AudioSource>().Play();
+                    waitingObjectsToPlay.Enqueue(playingObject);
+                }
+                startTime = Time.time;
+            }
+            lastTime = Time.time;
+        }
+    }
 
     public void handleCollision(GameObject collidedObject)
     {
         capturedSource.Play();
         Debug.Log("Captured");
-        float newX = Random.Range(-WorldSize / 2f, WorldSize / 2f);
-        float newY = Random.Range(-WorldSize / 2f, WorldSize / 2f);
-        float newZ = Random.Range(-WorldSize / 2f, WorldSize / 2f);
-        collidedObject.transform.position = new Vector3(newX, newY, newZ);
+        createNewFish();
+        Destroy(collidedObject);
     }
 
-    public void createNewFish()
+    public GameObject createNewFish()
     {
-        Debug.Log("Fish created");
-        GameObject newGoalObject = Instantiate(GoalObjects[0], transform);
+        int fishChoiceIndex = Random.Range(0, GoalObjects.Length);
+        GameObject newGoalObject = Instantiate(GoalObjects[fishChoiceIndex], transform);
         float newX = Random.Range(-WorldSize / 2f, WorldSize / 2f);
         float newY = Random.Range(-WorldSize / 2f, WorldSize / 2f);
         float newZ = Random.Range(-WorldSize / 2f, WorldSize / 2f);
         newGoalObject.transform.position = new Vector3(newX, newY, newZ);
-        currFish++;
+        newGoalObject.transform.Rotate(new Vector3(0f, 1f, 0f), Random.Range(0, 360));
+        if (objectsToPlay.Count > 0)
+            objectsToPlay.Enqueue(newGoalObject);
+        else
+            waitingObjectsToPlay.Enqueue(newGoalObject);
+        return newGoalObject;
     }
 }
